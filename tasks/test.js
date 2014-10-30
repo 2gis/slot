a2f5681beac43ffd47f989699c2bb864293eacf0
i.taratuhin <i.taratuhin@2gis.ru>
2014-10-30 15:58:06 +0600

    add code-coverage #75

--- tasks/test.js
+++ tasks/test.js
@@ -1,14 +1,27 @@
 var gulp = require('gulp');
 var mocha = require('gulp-mocha');
+var istanbul = require('gulp-istanbul');
 
-gulp.task('test', function() {
+gulp.task('test', ['cover'], function() {
     return gulp.src([
         'tests/unitTestConfig.js',
         '**/*.spec.js'
     ], {read: false})
         .pipe(mocha({
             globals: ['DEBUG']
+        }))
+        .pipe(istanbul.writeReports({
+            dir: './coverage',
+            reporters: [ 'lcov', 'text']
         }));
+});
+
+gulp.task('cover', function(){
+    return gulp.src([
+        '*.js',
+        'clientApp/*.js',
+        '!clientApp/*.spec.js'
+    ]).pipe(istanbul());
 });
 
 gulp.task('default', ['test']);
