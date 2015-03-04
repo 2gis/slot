00eae93ee0c83f9e530dad0256a22c2bf6e7eaa5
Kuznetsov Dmitry <dm.kuznecov@2gis.ru>
2015-03-04 11:22:46 +0600

    add/docs-concept

--- tasks/docs.js
+++ tasks/docs.js
@@ -3,27 +3,37 @@ var del = require('del');
 
 var gulp = require('gulp');
 var jsdoc = require('gulp-jsdoc');
+var docsFiles = [
+    'config.js',
+    'env.js',
+    'slot/index.js',
+    'moduleConstructor.js',
+    'app.js',
+    'serverApp.js',
+    'clientApp/index.js',
 
-gulp.task('docs', function() {
+    'lib/*.js',
+
+    'README.md'
+];
+
+gulp.task('docs', ['docs.build', 'docs.watch']);
+
+gulp.task('docs.build', function() {
     del.sync('docs/api');
 
-    return gulp.src([
-        'config.js',
-        'env.js',
-        'slot.js',
-        'moduleConstructor.js',
-        'app.js',
-        'serverApp.js',
-        'clientApp/index.js',
-
-        'lib/*.js',
-
-        'README.md'
-    ])
+    return gulp.src(docsFiles)
         .pipe(jsdoc.parser({
             plugins: ['plugins/markdown']
         }))
         .pipe(jsdoc.generator('./docs/api', {
+            path: 'ink-docstrap',
+            theme: 'spacelab'
+        }, {
             applicationName: 'slot'
         }));
+});
+
+gulp.task('docs.watch', function() {
+    gulp.watch(docsFiles, ['docs.build']);
 });
